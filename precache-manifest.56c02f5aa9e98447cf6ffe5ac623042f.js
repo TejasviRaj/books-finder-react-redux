23b0af294bf87181a8a7b2fa77dfcbbee303bdb2
Tejasvi Raj Pant <tejpant@gmail.com>
2020-07-15 01:18:28 +0545

    Updates

---
+++ precache-manifest.56c02f5aa9e98447cf6ffe5ac623042f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d9c02b2f42e75938f09f2391d4d985",
+    "url": "/books-finder-react-redux/index.html"
+  },
+  {
+    "revision": "96dacf2cb8f2677ba3eb",
+    "url": "/books-finder-react-redux/static/css/main.4025fe2e.chunk.css"
+  },
+  {
+    "revision": "82f40df8a2b357cd8dd2",
+    "url": "/books-finder-react-redux/static/js/2.f9b51d6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/books-finder-react-redux/static/js/2.f9b51d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dacf2cb8f2677ba3eb",
+    "url": "/books-finder-react-redux/static/js/main.867cd7e7.chunk.js"
+  },
+  {
+    "revision": "d467402cf1ae564e10d3",
+    "url": "/books-finder-react-redux/static/js/runtime-main.a0bc14b9.js"
+  }
+]);
